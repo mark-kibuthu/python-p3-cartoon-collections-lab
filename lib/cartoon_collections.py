6139061673ea15c786509cdeb253e86630c52d57
Agent-grey <markkibuthu@gmail.com>
2024-09-09 17:28:59 +0300

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,17 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for index, dwarf in enumerate(dwarves, start=1):
+        print(f"{index}. {dwarf}")
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [call.capitalize() + "!" for call in planeteer_calls]
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
+
+def find_the_cheese(ingredients):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for item in ingredients:
+        if item in cheeses:
+            return item
+    return None
